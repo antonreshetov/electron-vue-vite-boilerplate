b21cd878fce05cfadfefafdef5d92734e8764abc
Anton Reshetov <reshetov.art@gmail.com>
2025-08-10 07:06:51 +0300

    fix: update Vite config to use correct renderer path

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,11 @@ import path from 'node:path'
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 
-const pathSrc = path.resolve(__dirname, 'src/renderer')
 const rootSrc = path.resolve(__dirname, 'src')
+const rootRenderer = path.resolve(__dirname, 'src/renderer')
 
 export default defineConfig({
-  root: pathSrc,
+  root: rootRenderer,
   base: './',
   plugins: [vue()],
   build: {
@@ -15,7 +15,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': pathSrc,
+      '@': rootRenderer,
       '~': rootSrc,
     },
   },

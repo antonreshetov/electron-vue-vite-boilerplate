2fbfb566ac55517af7fb2d0f53b83789faa0c577
Anton Reshetov <reshetov.art@gmail.com>
2025-08-10 07:07:59 +0300

    build: update Vite config to add server watch options

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import path from 'node:path'
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 
+const root = path.resolve(__dirname)
 const rootSrc = path.resolve(__dirname, 'src')
 const rootRenderer = path.resolve(__dirname, 'src/renderer')
 
@@ -17,6 +18,15 @@ export default defineConfig({
     alias: {
       '@': rootRenderer,
       '~': rootSrc,
+    },
+  },
+  server: {
+    watch: {
+      ignored: [
+        `${root}/scripts/**/*`,
+        `${root}/build/**/*`,
+        `${root}/src/main/**/*`,
+      ],
     },
   },
 })
